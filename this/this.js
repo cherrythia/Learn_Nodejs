fd811d5d12db6e8f5d0bcdf3af51018394a78e45
Terry Chia <chiawz@hotmail.com>
2017-06-09 06:12:23 +0800

    understand concept of this.
    this refers to the calling context

---
+++ this/this.js
@@ -0,0 +1,16 @@
+var Bucky = {
+	printFirstName: function() {
+		console.log('name is bucky');
+		console.log(this === Bucky); //true, because Bucky is calling it.
+	}
+};
+
+Bucky.printFirstName();
+
+//default calling context is global
+function doSomethingUseless() {
+	console.log('\n I am worthless');
+	console.log(this === Bucky);
+}
+
+doSomethingUseless();
